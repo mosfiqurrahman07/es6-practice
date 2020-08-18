750bc4a195247e0b5acd2dfa32ac107f967c168c
nisho786 <61291182+nisho786@users.noreply.github.com>
2020-08-18 07:15:19 +0600

    es6 practice

---
+++ letconst.js
@@ -0,0 +1,21 @@
+const hubby = "nisho";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(54);
+console.log(numbers);
+
+const nayok = {name:"Sakib khan", phone:4578};
+console.log(nayok);
+
+
+let patientName = "Rahum Cacha";
+patientName = "Fatema khala";
+console.log(patientName);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(i);
---
+++ template.js
@@ -0,0 +1,21 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " " + "is a good boy";
+const fullName2 = `${firstName} ${20+50+30} ${lastName} is a good boy`;
+console.log(fullName2);
+
+
+const multiLine = "I love you\n"
+    + "I miss you\n"
+    + "I need you";
+
+const multiLine2 = `I Love you
+I miss you 
+no. I don't need you
+Baily road e dorkar nai`;
+console.log(multiLine2);
+
+
+const name = 'Adam Sandler';
+const greeting = 'Hello adam Sandler, welcome to the club';
+console.log(greeting);
